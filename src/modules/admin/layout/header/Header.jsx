1510a3ec3b570b670b2a222b68e0d1fc09d6004e
LokeshK <Lokesh.Krishnamoorthy@intergy.com.au>
2022-05-23 09:32:59 +0530

    input

--- src/modules/admin/layout/header/Header.jsx
+++ src/modules/admin/layout/header/Header.jsx
@@ -1,114 +1,98 @@
 import { styled } from "@mui/material/styles";
 import React from "react";
-import {
-  Box,
-  Divider,
-  InputAdornment,
-  Stack,
-  TextField,
-  Typography,
-} from "@mui/material";
+import { Box, Divider, InputAdornment, Stack, TextField, Typography } from "@mui/material";
 import palette from "../../../../theme/palette";
 import AppIcons, { HomeIcon } from "../../../../constants/icons";
 import { StyledInput } from "../../../../theme/GlobalStyles";
 
 const Header = () => {
-  return (
-    <Root>
-      {/* Page title */}
-      <PageTitle variant="h6" color="primary" fontWeight={"600"}>
-        Project Planning
-      </PageTitle>
-      {/* SearchBar */}
-      <SearchBar
-        placeholder="search"
-        variant="outlined"
-        InputProps={{
-          endAdornment: (
-            <InputAdornment position="end">
-              <img src={AppIcons.Search} alt="search" />
-            </InputAdornment>
-          ),
-        }}
-      />
+	return (
+		<Root>
+			{/* Page title */}
+			<PageTitle variant="h6" color="primary" fontWeight={"600"}>
+				Project Planning
+			</PageTitle>
+			{/* SearchBar */}
+			<SearchBar
+				placeholder="search"
+				variant="outlined"
+				InputProps={{
+					endAdornment: (
+						<InputAdornment position="end">
+							<img src={AppIcons.Search} alt="search" />
+						</InputAdornment>
+					),
+				}}
+			/>
 
-      {/* Breadcrumb */}
-      <BreadcrumbWrapper
-        direction={"row"}
-        alignItems="center"
-        justifyContent={"space-evenly"}
-      >
-        <BreadcrumbItem>
-          <HomeIcon bg={palette.grey[400]} />
-        </BreadcrumbItem>
-        <BreadcrumbDivider orientation="vertical" variant="middle" flexItem />
-        <BreadcrumbItem>
-          <Typography className="title" variant="body2" fontWeight={"bold"}>
-            Add Project
-          </Typography>
-        </BreadcrumbItem>
-        <BreadcrumbDivider orientation="vertical" variant="middle" flexItem />
-        <BreadcrumbItem active>
-          <Typography
-            active
-            className="title"
-            variant="body2"
-            fontWeight={"bold"}
-          >
-            Project Planning
-          </Typography>
-        </BreadcrumbItem>
-      </BreadcrumbWrapper>
-    </Root>
-  );
+			{/* Breadcrumb */}
+			<BreadcrumbWrapper direction={"row"} alignItems="center" justifyContent={"space-evenly"}>
+				<BreadcrumbItem>
+					<HomeIcon bg={palette.grey[400]} />
+				</BreadcrumbItem>
+				<BreadcrumbDivider orientation="vertical" variant="middle" flexItem />
+				<BreadcrumbItem>
+					<Typography className="title" variant="body2" fontWeight={"bold"}>
+						Add Project
+					</Typography>
+				</BreadcrumbItem>
+				<BreadcrumbDivider orientation="vertical" variant="middle" flexItem />
+				<BreadcrumbItem active>
+					<Typography active className="title" variant="body2" fontWeight={"bold"}>
+						Project Planning
+					</Typography>
+				</BreadcrumbItem>
+			</BreadcrumbWrapper>
+		</Root>
+	);
 };
 
 export default Header;
 
 const Root = styled(Box)(({ theme }) => ({
-  backgroundColor: "transparent",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "space-between",
-  marginBottom: theme.spacing(1.2),
+	backgroundColor: "transparent",
+	display: "flex",
+	alignItems: "center",
+	justifyContent: "space-between",
+	marginBottom: theme.spacing(1.2),
 }));
 
 const SearchBar = styled(StyledInput)(({ theme }) => ({
-  maxWidth: "550px",
+	maxWidth: "600px",
 }));
 
 const PageTitle = styled(Typography)(({ theme }) => ({
-  marginLeft: 40,
-  marginRight: 15,
-  [theme.breakpoints.down("md")]: {
-    marginLeft: 30,
-    marginRight: 10,
-    fontSize: 16,
-  },
+	marginLeft: 40,
+	marginRight: 15,
+	[theme.breakpoints.down("md")]: {
+		marginLeft: 30,
+		marginRight: 10,
+		fontSize: 16,
+	},
 }));
 
 const BreadcrumbWrapper = styled(Stack)(({ theme }) => ({}));
 
 const BreadcrumbItem = styled(Typography)(({ theme, active }) => ({
-  paddingInline: theme.spacing(2),
-  [theme.breakpoints.down("md")]: {
-    paddingInline: theme.spacing(1.2),
-  },
-  paddingRight: active ? 0 : theme.spacing(2),
-  "&:first-child": {
-    paddingInline: theme.spacing(3),
-    [theme.breakpoints.down("md")]: {
-      paddingInline: theme.spacing(2),
-    },
-  },
-  "& .title": {
-    color: active ? palette.primary.main : palette.grey[400],
-    gap: theme.spacing(1),
-  },
+	paddingInline: theme.spacing(2),
+	[theme.breakpoints.down("md")]: {
+		paddingInline: theme.spacing(1.2),
+	},
+	paddingRight: active ? 0 : theme.spacing(2),
+	"&:first-child": {
+		paddingInline: theme.spacing(3),
+		[theme.breakpoints.down("md")]: {
+			paddingInline: theme.spacing(2),
+		},
+	},
+	"& .title": {
+		color: active ? palette.primary.main : palette.grey[400],
+		gap: theme.spacing(1),
+	},
 }));
 const BreadcrumbDivider = styled(Divider)(({ theme, active }) => ({
-  height: 15,
-  backgroundColor: palette.border,
-  borderWidth: 0.5,
-  marginBlock: 5,
+	height: 15,
+	backgroundColor: palette.border,
+	borderWidth: 0.5,
+	marginBlock: 5,
 }));
--- src/theme/GlobalStyles.js
+++ src/theme/GlobalStyles.js
@@ -3,37 +3,37 @@ import { styled } from "@mui/material/styles";
 import palette from "./palette";
 
 export const StyledBox = styled(Box)(({ theme }) => ({
-    minHeight: "60px",
-    backgroundColor: palette.common.white,
-    padding: theme.spacing(1),
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "flex-start",
-    borderRadius: 5,
-    boxShadow: "0px 0px 4px rgba(166, 166, 166, 0.25)",
-    gap: theme.spacing(3),
+	minHeight: "60px",
+	backgroundColor: palette.common.white,
+	padding: theme.spacing(1),
+	display: "flex",
+	alignItems: "center",
+	justifyContent: "flex-start",
+	borderRadius: 5,
+	boxShadow: "0px 0px 4px rgba(166, 166, 166, 0.25)",
+	gap: theme.spacing(3),
 }));
 
 export const StyledBtn = styled(Button)(({ theme }) => ({
-    padding: theme.spacing(0.9),
-    paddingInline: theme.spacing(2),
+	padding: theme.spacing(0.9),
+	paddingInline: theme.spacing(2),
 }));
 export const StyledCheckBoxWrapper = styled(FormGroup)(({ theme }) => ({
-    "& .MuiFormControlLabel-root ": {
-        marginLeft: 0,
-    },
+	"& .MuiFormControlLabel-root ": {
+		marginLeft: 0,
+	},
 }));
 
 export const StyledInput = styled(TextField)(({ theme }) => ({
-    flex: 1,
-    width: "100%",
-    borderRadius: 5,
-    backgroundColor: palette.common.white,
-    borderColor: palette.border,
+	flex: 1,
+	width: "100%",
+	borderRadius: 5,
+	backgroundColor: palette.common.white,
+	borderColor: palette.border,
 
-    "& .MuiOutlinedInput-input": {
-        padding: theme.spacing(1),
-        fontSize: 14,
-        color: palette.grey[600],
-    },
+	"& .MuiOutlinedInput-input": {
+		padding: theme.spacing(1.2),
+		fontSize: 14,
+		color: palette.grey[600],
+	},
 }));
